33734b23f3adb0caca259d85ce8def67cfa93b2d
zGuian <guianmarcos32@gmail.com>
2023-10-02 16:54:58 -0300

    Config inicial conexão banco de dados

--- API_TeamsData/Data/ApiContext.cs
+++ API_TeamsData/Data/ApiContext.cs
@@ -1,7 +1,12 @@
-﻿namespace API_TeamsData.Data
+﻿using API_TeamsData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_TeamsData.Data
 {
     public class ApiContext : DbContext
     {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
 
+        public DbSet<Times> Times { get; set; }
     }
 }
--- API_TeamsData/Program.cs
+++ API_TeamsData/Program.cs
@@ -1,6 +1,12 @@
+using API_TeamsData.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApiContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
